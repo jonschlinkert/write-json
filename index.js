eecb6649856ba71b226454b54ab7025efe446db4
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-12 23:06:31 -0400

    update to support all `JSON.stringify` args

--- index.js
+++ index.js
@@ -13,17 +13,22 @@ var writeFile = require('write');
  * Expose `writeJson`
  */
 
-module.exports = function writeJson(dest, obj, cb) {
-  writeFile(dest, JSON.stringify(obj, null, 2), cb);
+module.exports = function writeJson(dest, data, cb) {
+  var args = [].slice.call(arguments, 1);
+  cb = args.pop();
+  if (args.length === 1) args.push(null, 2);
+  writeFile(dest, JSON.stringify.apply(JSON, args), cb);
 };
 
 /**
  * Expose `writeJson.sync`
  */
 
-module.exports.sync = function writeJsonSync(dest, obj) {
+module.exports.sync = function writeJsonSync(dest, data) {
   try {
-    writeFile.sync(dest, JSON.stringify(obj, null, 2));
+    var args = [].slice.call(arguments, 1);
+    if (args.length === 1) args.push(null, 2);
+    writeFile.sync(dest, JSON.stringify.apply(JSON, args));
   } catch (err) {
     throw err;
   }
--- test.js
+++ test.js
@@ -6,16 +6,27 @@ var assert = require('assert');
 var writeJson = require('./');
 var del = require('delete');
 
-describe('write-json', function () {
-  afterEach(function (cb) {
+describe('write-json', function() {
+  afterEach(function(cb) {
     del('actual', cb);
   });
 
-  describe('async', function () {
-    it('should write JSON asyncronously', function (cb) {
+  describe('async', function() {
+    it('should write JSON asyncronously', function(cb) {
       var expected = {foo: {bar: "baz"} };
-      writeJson('actual/test.json', expected, function (err) {
-        fs.readFile('actual/test.json', 'utf8', function (err, res) {
+      writeJson('actual/test.json', expected, function(err) {
+        fs.readFile('actual/test.json', 'utf8', function(err, res) {
+          if (err) return cb(err);
+          assert.deepEqual(JSON.parse(res), expected);
+          cb();
+        });
+      });
+    });
+
+    it('should take additional JSON.stringify args', function(cb) {
+      var expected = {foo: {bar: "baz"} };
+      writeJson('actual/test.json', expected, null, 0, function(err) {
+        fs.readFile('actual/test.json', 'utf8', function(err, res) {
           if (err) return cb(err);
           assert.deepEqual(JSON.parse(res), expected);
           cb();
@@ -24,10 +35,18 @@ describe('write-json', function () {
     });
   });
 
-  describe('sync', function () {
-    it('should write JSON syncronously', function (cb) {
+  describe('sync', function() {
+    it('should write JSON syncronously', function(cb) {
       var expected = {foo: {bar: "baz"} };
       writeJson.sync('actual/test.json', expected);
+      var res = fs.readFileSync('actual/test.json', 'utf8');
+      assert.deepEqual(JSON.parse(res), expected);
+      cb();
+    });
+
+    it('should additional JSON.stringify args', function(cb) {
+      var expected = {foo: {bar: "baz"} };
+      writeJson.sync('actual/test.json', expected, null, 0);
       var res = fs.readFileSync('actual/test.json', 'utf8');
       assert.deepEqual(JSON.parse(res), expected);
       cb();
